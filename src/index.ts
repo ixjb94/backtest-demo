ded97b46cffd6f5f50ea4259705149891c93167a
IX JB <ixjb94@gmail.com>
2023-02-19 09:42:45 +0000

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { BackTest } from "./core/backtest"
 export { CsvParser } from "./core/csvparser"
+
+export {
+	BackTestOptions, CrossFunction, Enter, EnterFunction,
+	Exit, ExitFunction, GenericObject, HistoryType, Orders,
+	SideOptions, SideType, Strategy, TradeType, Trades, WinLossType
+ } from "./types/backtest"
